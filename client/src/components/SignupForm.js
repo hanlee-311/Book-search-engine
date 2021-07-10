8212bfa262f2535c10f88945bd78177674717021
Hannah Lee <013hannah.a.lee@gmail.com>
2021-07-09 21:55:47 -0500

    fixed error for deployment

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { CREATE_USER } from '../utils/mutations';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { useMutation, useQuery } from '@apollo/client';
 
@@ -13,7 +13,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser, {error}] = useMutation(CREATE_USER);
+  const [addUser, {error}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -32,8 +32,8 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({ variables: userFormData });
-      
+      const { data } = await addUser({ variables: userFormData });
+
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
