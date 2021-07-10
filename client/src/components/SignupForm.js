d8d8c6f6df9285ff27eed36581ae0a90a8447d42
Hannah Lee <013hannah.a.lee@gmail.com>
2021-07-10 09:17:59 -0500

    err fixing

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -33,6 +33,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({ variables: userFormData });
+      console.log(data)
 
       Auth.login(data.addUser.token);
     } catch (err) {
