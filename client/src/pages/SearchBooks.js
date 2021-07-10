e06dd8334cb3d485bfda16279a49e181a7d03b17
Hannah Lee <013hannah.a.lee@gmail.com>
2021-07-09 22:31:45 -0500

    attempting to fix error with bookId being undefined

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -74,6 +74,8 @@ const SearchBooks = () => {
         throw new Error('something went wrong!');
       }
 
+      console.log(bookToSave.bookId)
+
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
